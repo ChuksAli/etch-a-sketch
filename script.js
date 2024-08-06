2d791e2a00537743c30005e9353eb97639d5383a
Chuks Alichukwu <[alichukwucp@gmail.com]>
2024-08-06 19:18:32 +0100

    Add "drawing effect to grid - Change grid colour when mouse enters a grid to emulate a sketchboard

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ for(i=0; i<16 * 16; i++){
     const grid = document.createElement("div")
     grid.classList.add("grid")
     container.appendChild(grid);
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = 'darkgray';
+    });
 }
+
