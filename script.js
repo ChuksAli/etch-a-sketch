8c86b2edec533d8c33f9e75ae3136b5164d8dba6
Chuks Alichukwu <[alichukwucp@gmail.com]>
2024-08-06 19:31:29 +0100

    Add color onnly when mouse down -Use Boolean statements to only add pen when mouse is down to avoid mistakes

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const container = document.querySelector(".container");
+
+let isMouseDown = false;
+container.addEventListener("mousedown", () => isMouseDown = true);
+container.addEventListener("mouseup", () => isMouseDown = false);
 for(i=0; i<16 * 16; i++){
     const grid = document.createElement("div")
     grid.classList.add("grid")
     container.appendChild(grid);
     grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = 'darkgray';
-    });
+        if(isMouseDown){
+            grid.style.backgroundColor = 'darkgray';
+    }});
 }
 
