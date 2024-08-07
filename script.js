5a6c553d3b5ea7371f65225e6261cd632d3a0017
Chuks Alichukwu <[alichukwucp@gmail.com]>
2024-08-07 10:36:26 +0100

    Add Ability to choose grid length size - Add javascript prompt to take desired length size from user - Save grid size to variable and use css styling in JS code to make this the grid size - Ensure input is a number and within allower range

--- script.js
+++ script.js
@@ -3,13 +3,38 @@ const container = document.querySelector(".container");
 let isMouseDown = false;
 container.addEventListener("mousedown", () => isMouseDown = true);
 container.addEventListener("mouseup", () => isMouseDown = false);
-for(i=0; i<16 * 16; i++){
-    const grid = document.createElement("div")
-    grid.classList.add("grid")
-    container.appendChild(grid);
-    grid.addEventListener("mouseover", () => {
-        if(isMouseDown){
-            grid.style.backgroundColor = 'darkgray';
-    }});
-}
+
+const button = document.querySelector(".grid-size");
+button.addEventListener("click", () => {
+   let length = prompt("What length size grid would you like? (100 Max)")
+    if(isNaN(length)){
+        alert("Input must be a valid number");
+    }else if(length > 100 || length < 1){
+        alert("Grid length must be between 0 and 101")
+    }
+
+    for(i=0; i<length * length; i++){
+        const grid = document.createElement("div")
+        grid.classList.add("grid")
+        grid.style.width = `calc(100% / ${length})`;
+        grid.style.length = `calc(100% / ${length})`;
+        container.appendChild(grid);
+    
+        grid.addEventListener("mouseover", () => {
+            if(isMouseDown){
+                grid.style.backgroundColor = 'darkgray';
+        }
+    
+    });
+    
+        grid.addEventListener("mousedown", () => {
+            grid.style.backgroundColor = 'darkgray'
+        })
+    }
+});
+
+
+
+
+
 
