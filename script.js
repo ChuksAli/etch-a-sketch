a29bde6552b02b9b6d4ee30d2eea1294976725d4
Chuks Alichukwu <[alichukwucp@gmail.com]>
2024-08-07 11:02:28 +0100

    Clear grid before new grid length input -Seperate out createGrid function for improved readability -Clear container innerHTML before inputting new grid length

--- script.js
+++ script.js
@@ -5,33 +5,43 @@ container.addEventListener("mousedown", () => isMouseDown = true);
 container.addEventListener("mouseup", () => isMouseDown = false);
 
 const button = document.querySelector(".grid-size");
-button.addEventListener("click", () => {
-   let length = prompt("What length size grid would you like? (100 Max)")
-    if(isNaN(length)){
-        alert("Input must be a valid number");
-    }else if(length > 100 || length < 1){
-        alert("Grid length must be between 0 and 101")
-    }
 
-    for(i=0; i<length * length; i++){
-        const grid = document.createElement("div")
-        grid.classList.add("grid")
-        grid.style.width = `calc(100% / ${length})`;
-        grid.style.length = `calc(100% / ${length})`;
-        container.appendChild(grid);
-    
-        grid.addEventListener("mouseover", () => {
-            if(isMouseDown){
-                grid.style.backgroundColor = 'darkgray';
-        }
-    
-    });
-    
-        grid.addEventListener("mousedown", () => {
-            grid.style.backgroundColor = 'darkgray'
+function createGrid(length){ 
+     
+        container.innerHTML = '';
+         for(i=0; i<length * length; i++){
+             const grid = document.createElement("div")
+             grid.classList.add("grid")
+             grid.style.width = `calc(100% / ${length})`;
+             grid.style.length = `calc(100% / ${length})`;
+             container.appendChild(grid);
+         
+             grid.addEventListener("mouseover", () => {
+                 if(isMouseDown){
+                     grid.style.backgroundColor = 'darkgray';
+             }
+         
+         });
+         
+             grid.addEventListener("mousedown", () => {
+                 grid.style.backgroundColor = 'darkgray'
+             })
+         }
+     
+     };
+
+     button.addEventListener("click", () => {
+        let length = prompt("What length size grid would you like? (100 Max)")
+         if(isNaN(length)){
+             alert("Input must be a valid number");
+         }else if(length > 100 || length < 1){
+             alert("Grid length must be between 0 and 101")
+         } else{
+            createGrid(length);
+         }
         })
-    }
-});
+
+
 
 
 
